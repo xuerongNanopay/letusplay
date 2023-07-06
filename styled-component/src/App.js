c66ae20b8b2f0292e6a01a132f0e02cc69df1028
xuerongNanopay <xuerong@nanopay.net>
2023-07-06 12:19:21 -0400

    add header and container

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -1,8 +1,13 @@
+import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      aaa
-    </div>
+    <>
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
   );
 }
 
---
+++ styled-component/src/components/Header.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styles/Header.styled"
+
+export default function Header() {
+  return (
+    <StyledHeader bg="red">
+      <h1>Header</h1>
+    </StyledHeader>
+  )
+}
