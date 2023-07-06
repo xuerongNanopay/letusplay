72555ef04acbcff5e269f62648882ea519e106d5
xuerongNanopay <xuerong@nanopay.net>
2023-07-06 19:41:30 -0400

    add header

--- styled-component/src/components/Header.js
+++ styled-component/src/components/Header.js
@@ -1,6 +1,7 @@
 import { Container } from "./styles/Container.styled"
-import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled"
 import { Button } from "./styles/Button.styled"
+import { Flex } from "./styles/Flex.styles"
 
 export default function Header() {
   return (
@@ -10,6 +11,14 @@ export default function Header() {
           <div><Logo src='./images/logo.svg' alt='' /></div>
           <div><Button>Try It Free adsfd</Button></div>
         </Nav>
+        <Flex>
+          <div>
+            <h1>Laboris aliqua anim deserunt enim incididunt esse sunt.</h1>
+            <p>Est duis non aliquip aliquip magna laboris. Occaecat dolore esse occaecat nisi aliquip nostrud anim. Quis ea adipisicing sunt laboris excepteur magna amet esse. Nostrud labore tempor consequat elit ad. Minim consequat incididunt cillum velit aliqua est occaecat elit. Cupidatat tempor sit Lorem elit elit velit do fugiat occaecat culpa.</p>
+          </div>
+
+          <Image src="./images/illustration-mockups.svg" alt=""></Image>
+        </Flex>
       </Container>
     </StyledHeader>
   )
