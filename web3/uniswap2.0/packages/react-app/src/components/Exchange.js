662f36b012edd7e0c9a68440d96b7202c12db301
xuerongNanopay <xuerong@nanopay.net>
2023-07-28 10:58:38 -0400

    add imports for Exchanges.js

--- web3/uniswap2.0/packages/react-app/src/components/Exchange.js
+++ web3/uniswap2.0/packages/react-app/src/components/Exchange.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react'
 
 import { Contract } from '@ethersproject/contracts'
+import { abis } from '@my-app/contracts'
+import { 
+  ERC20, 
+  useContractFunction,
+  useEthers,
+  useTokenAllowance,
+  useTokenBalance
+} from '@usedapp/core'
+import { ethers } from 'ethers'
+import { parseUnits } from 'ethers/lib/utils'
 
 function Exchange() {
   return (
