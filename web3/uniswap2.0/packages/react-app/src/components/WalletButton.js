901d746ff76155112ce5392bbc46c6425708cb6c
xuerongNanopay <xuerong@nanopay.net>
2023-07-27 20:38:54 -0400

    Finish wallet button

--- web3/uniswap2.0/packages/react-app/src/components/WalletButton.js
+++ web3/uniswap2.0/packages/react-app/src/components/WalletButton.js
@@ -4,10 +4,19 @@ import { shortenAddress, useEthers, useLookupAddress } from '@usedapp/core'
 import styles from '../styles'
 
 const WalletButton = () => {
-  const [ rendered, setRendered] = useState('')
+  const [ accountAddress, setAccountAddress] = useState('')
   const { ens } = useLookupAddress()
-  const { account, activateBrowserWallet, deactive } = useEthers()
+  const { account, activateBrowserWallet, deactivate } = useEthers()
 
+  useEffect(() => {
+    if ( ens ) {
+      setAccountAddress(ens)
+    } else if ( account ) {
+      setAccountAddress(shortenAddress(account))
+    } else {
+      setAccountAddress('')
+    }
+  }, [account, ens, setAccountAddress])
 
   return (
     <button
@@ -15,13 +24,12 @@ const WalletButton = () => {
         if ( ! account ) {
           activateBrowserWallet()
         } else {
-          deactive()
+          deactivate()
         }
       }}
       className={styles.walletButton}
     >
-      { rendered === '' && 'Connect Wallet' }
-      { rendered !== '' && rendered }
+      { accountAddress || 'Connect Wallet' }
     </button>
   )
 }
