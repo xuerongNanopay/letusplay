67e1288811cf3e2d45ae65ecb2dd3b0f371f4c59
xuerongNanopay <xuerong@nanopay.net>
2023-07-29 22:54:47 -0400

    initial profile

--- nextJS_project/promptopia/app/profile/page.jsx
+++ nextJS_project/promptopia/app/profile/page.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { userRouter } from 'next/navigation'
 
 const Profile = () => {
   return (
