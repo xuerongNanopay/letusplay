05c27e260ab96aaecb0237c449998ab48597693e
xuerongNanopay <xuerong@nanopay.net>
2023-07-31 20:38:01 -0400

    add delete post function

--- nextJS_project/promptopia/app/profile/page.jsx
+++ nextJS_project/promptopia/app/profile/page.jsx
@@ -17,7 +17,18 @@ const MyProfile = () => {
   }
 
   const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
 
+    if ( hasConfirmed ) {
+      try {
+        await fetch(`/api/prompt/${post._id}`, {method: 'DELETE'})
+
+        const filteredPosts = posts.filter(p => p._id !== post._id)
+        setPosts(filteredPosts)
+      } catch ( err ) {
+        console.log(err)
+      }
+    }
   }
 
   useEffect(() => {
