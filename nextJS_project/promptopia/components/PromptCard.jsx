8b4182ef1d51f25ab47f0d9dcd7521fedbaf6292
xuerongNanopay <xuerong@nanopay.net>
2023-07-31 20:31:30 -0400

    remove simple on the #

--- nextJS_project/promptopia/components/PromptCard.jsx
+++ nextJS_project/promptopia/components/PromptCard.jsx
@@ -65,7 +65,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={_ => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
       {
         session?.user.id === post.creator._id && 
