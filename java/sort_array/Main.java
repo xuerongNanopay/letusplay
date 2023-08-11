43ec40dabd889c532baae320c40fc270c43230fe
xuerongNanopay <xuerong@nanopay.net>
2023-08-11 17:38:45 -0400

    save

--- java/sort_array/Main.java
+++ java/sort_array/Main.java
@@ -1,7 +1,45 @@
 // Sort Array demo.
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.LongStream;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Sort Array");
+    //Initial to all 0.
+    long[] longs = new long[10];
+    printAll(longs);
+    br();
+
+    Arrays.fill(longs, 1L);
+    printAll(longs);
+    br();
+
+    long[] rangLongs = LongStream.rangeClosed(10, 20).toArray();
+    printAll(rangLongs);
+    br();
+
+    Long[] rangeBoxLongs = Arrays.stream(rangLongs).boxed().sorted(Collections.reverseOrder()).toArray(Long[]::new);
+    printAll(rangeBoxLongs);
+    br();
+
+    System.out.println("Is 15 in the array: " + Arrays.binarySearch(rangLongs, 15));
+    System.out.println("Is 30 in the array: " + Arrays.binarySearch(rangLongs, 30));
   }
+
+  public static void br() {
+    System.out.println();
+  }
+
+  public static void printAll(long[] arrs) {
+    for ( int i = 0 ; i < arrs.length ; i++ ) {
+      System.out.print(arrs[i] + " ");
+    }
+  }
+
+  public static <T> void printAll(T[] arrs) {
+    for ( int i = 0 ; i < arrs.length ; i++ ) {
+      System.out.print(arrs[i] + " ");
+    }
+  }
+
 }
