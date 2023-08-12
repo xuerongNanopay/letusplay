9d36a9123a88d4ab27f808aed1f22ef23dd1d1b2
xuerongNanopay <xuerong@nanopay.net>
2023-08-11 23:10:03 -0400

    add project

---
+++ canvas/collision/src/main.js
@@ -0,0 +1,10 @@
+console.log('aaaa')
+document.querySelector('#app').innerHTML = `
+  <div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ canvas/collision/vite.config.js
@@ -0,0 +1,9 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../target'
+  }
+})
