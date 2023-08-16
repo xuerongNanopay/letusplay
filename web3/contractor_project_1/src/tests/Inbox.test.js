0bb4d5c731f7d4317cb133f66193c75bc4b10a96
xuerongNanopay <xuerong@nanopay.net>
2023-08-16 09:30:05 -0400

    save update

--- web3/contractor_project_1/src/tests/Inbox.test.js
+++ web3/contractor_project_1/src/tests/Inbox.test.js
@@ -4,6 +4,7 @@ import Web3 from 'web3'
 import Inbox from '../compile.js'
 
 const web3 = new Web3(Ganache.provider())
+const INITIAL_MESSAGE="Hi there!"
 
 let fetchAccounts;
 let inbox;
@@ -18,7 +19,7 @@ beforeEach(async() => {
     inbox = await new web3
                         .eth
                         .Contract(JSON.parse(api))
-                        .deploy({data: bytecode, arguments: ['Hi there!']})
+                        .deploy({data: bytecode, arguments: [INITIAL_MESSAGE]})
                         .send({ from: fetchAccounts[0], gas: '1000000' })
   } catch ( err ) {
     console.log(err)
@@ -28,6 +29,19 @@ beforeEach(async() => {
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox)
+    // console.log(inbox);
+    assert.ok(inbox.options.address)
+  })
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, INITIAL_MESSAGE)
+    console.log(message)
+  })
+
+  it('can change the message', async() => {
+    await inbox.methods.setMessage('Bye').send({from: fetchAccounts[0]})
+    const message = await inbox.methods.message().call()
+    assert(message, 'bye')
   })
 })
